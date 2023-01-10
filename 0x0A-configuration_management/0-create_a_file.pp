9758df122f1cb0addc6e957305d562ba5257c0fe
SE-KIRIRO <lawrencekiriro@gmail.com>
2023-01-10 10:00:24 +0300

    creating file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# using puppet, create a file in /tmp 
+
+file { 'school':
+ensure	=> 'present',
+path	=> '/tmp/school',
+mode	=> '0744',
+owner	=> 'www-data',
+group	=> 'www-data',
+content	=> 'I love Puppet'
+}
