4237d17fdcec4d02b5375fffe6379027f744ced1
SE-KIRIRO <lawrencekiriro@gmail.com>
2023-01-11 20:08:53 +0300

    puppet-lint manenos

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# using puppet, create a file in /tmp 
+# using puppet, create a file in /tmp directory
 
 file { 'school':
-ensure	=> 'present',
-path	=> '/tmp/school',
-mode	=> '0744',
-owner	=> 'www-data',
-group	=> 'www-data',
-content	=> 'I love Puppet'
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
